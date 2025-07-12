1bce844f9b6ad8d13f3afe13735d46ee8f5cf362
Devonnnnnnnnnn <devonhandler@outlook.com>
2025-07-12 18:57:55 +0200

    Add files via upload

---
+++ events/messageCreate.js
@@ -0,0 +1,26 @@
+const usersData = require("../users.json"); // or your in-memory object
+const { saveUsers } = require("../utils");
+
+module.exports = {
+  name: "messageCreate",
+  async execute(message) {
+    if (message.author.bot) return;
+
+    const userId = message.author.id;
+    const user = usersData[userId];
+    if (!user) return; // no profile
+
+    user.xp += 5;
+
+    // Level up check
+    while (user.xp >= user.xpRequired) {
+      user.xp -= user.xpRequired;
+      user.level++;
+      user.xpRequired = Math.floor(user.xpRequired * 1.15);
+      user.coins += 50;
+      message.channel.send(`🎉 Congrats ${message.author}, you leveled up to **${user.level}**!`);
+    }
+
+    saveUsers(usersData);
+  },
+};
