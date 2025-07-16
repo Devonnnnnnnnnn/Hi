995eee2edeae74d1494b73e57304ab5c7c07a108
Devonnnnnnnnnn <devonhandler@outlook.com>
2025-07-16 18:34:32 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,11 +2,13 @@ module.exports = {
   name: "ping",
   description: "Check latency.",
   async execute(message) {
-    const msg = await message.channel.send("🏓 Pinging...");
-    const latency = msg.createdTimestamp - message.createdTimestamp;
+    const sentMsg = await message.channel.send("🏓 Pinging...");
+    const latency = sentMsg.createdTimestamp - message.createdTimestamp;
     const apiLatency = Math.round(message.client.ws.ping);
-    await msg.edit(
-      `🏓 Pong!\n🕑 Latency: **${latency}ms**\n🌐 API: **${apiLatency}ms**`
+    await sentMsg.edit(
+      `🏓 **Pong!**\n` +
+      `🕑 Latency: **${latency}ms**\n` +
+      `🌐 API: **${apiLatency}ms**`
     );
   },
 };
