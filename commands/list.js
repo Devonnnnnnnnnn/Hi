169daddedc6f93266a384ba8b132332af771a0d2
Devonnnnnnnnnn <devonhandler@outlook.com>
2025-07-16 16:20:19 +0200

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder } = require("discord.js");
 const styleStats = require("../data/styleStats.js");
 
 module.exports = {
@@ -5,13 +6,40 @@ module.exports = {
   description: "List all styles.",
   async execute(message) {
     const styles = Object.keys(styleStats).sort();
-    const formatted = styles.join(", ");
 
-    // If too long, paginate or just send summary
-    if (formatted.length > 2000) {
-      return message.channel.send("Too many styles to list.");
+    const embed = new EmbedBuilder().setColor(0x0099ff).setTitle("📋 Styles List");
+
+    const maxFieldLength = 1024; // Discord max field value length
+
+    // Split styles into chunks that fit into embed fields
+    let chunk = [];
+    let chunkLength = 0;
+
+    for (const style of styles) {
+      const toAdd = (chunk.length ? ", " : "") + style;
+      if (chunkLength + toAdd.length > maxFieldLength) {
+        embed.addFields({
+          name: "\u200B", // empty field name
+          value: chunk.join(", "),
+          inline: false,
+        });
+        chunk = [style];
+        chunkLength = style.length;
+      } else {
+        chunk.push(style);
+        chunkLength += toAdd.length;
+      }
     }
 
-    return message.channel.send(`📋 Styles:\n${formatted}`);
+    // Add the last chunk
+    if (chunk.length) {
+      embed.addFields({
+        name: "\u200B",
+        value: chunk.join(", "),
+        inline: false,
+      });
+    }
+
+    return message.channel.send({ embeds: [embed] });
   },
 };
