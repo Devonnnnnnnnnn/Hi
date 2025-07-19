f9d29ee12b1506dbab5172e1055b2b7d3d346b96
Devonnnnnnnnnn <devonhandler@outlook.com>
2025-07-19 12:42:17 +0200

    Update supabaseClient.js

--- events/supabaseClient.js
+++ events/supabaseClient.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = "https://fpvpdpiblzbtniayptnj.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwdnBkcGlibHpidG5pYXlwdG5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MjA4MDAsImV4cCI6MjA2ODQ5NjgwMH0.kItNXL58CCEjsxI0z2vF_r0mkMWhdTyR0UqzaZnCGf8; // keep this private and load from env in prod!
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
 module.exports = supabase;
